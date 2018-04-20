3ddef3cc0302b9a2b0c9dc32b6f6328502f08002
Evan Magoni <esm367@gmail.com>
2018-04-20 09:52:50 -0400

    add tmux symlink

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -4,4 +4,5 @@ ln -s $CWD/.vim ~/.vim
 mkdir -p ~/.config
 ln -s $CWD/.config/fish ~/.config/fish
 ln -s $CWD/.config/base16-shell/ ~/.config/base16-shell
+ln -s $CWD/.tmux.conf ~/.tmux.conf
 ln -s $CWD/Code/User ~/Library/Application\ Support/Code/User
