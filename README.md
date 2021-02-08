c5da83155829dfb03f94e920ce264c553efe5512
magoni <esm367@gmail.com>
2021-02-08 14:13:26 -0500

    Add apps.sh

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 # magoni
+
 My dotfiles.
 
 ## Setup
+
+1.
+
 ```
 git submodule update
 ./create-symlinks.sh
 ```
+
+2. https://brew.sh/ + XCode command-line tools if prompted
+
+3. `./macos.sh`
+
+4. `./apps.sh`
+
+5.
+
 ```
 # get latest vim on macOS
 brew install vim
 vi --version | head
-sudo mv /usr/bin/vi /usr/bin/vi-7.4
+sudo mv /usr/bin/vi /usr/bin/vi-default
 sudo ln -s /usr/local/bin/vim /usr/local/bin/vi
 ```
-```
-# project search utility (provides /usr/local/bin/ag)
-brew install the-silver-searcher
-```
-[nvm wrapper for fish](https://github.com/brigand/fast-nvm-fish)
+
+6. [nvm wrapper for fish](https://github.com/brigand/fast-nvm-fish)
---
+++ apps.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+brew tap homebrew/cask-fonts
+brew install font-hack
+
+brew install --cask karabiner-elements
+brew install --cask rectangle
+brew install --cask firefox
+brew install --cask slack
+brew install --cask google-chrome
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask zoom
+brew install --cask flycut
+brew install --cask hammerspoon
+brew install --cask balance-lock
+brew install --cask appcleaner
+brew install --cask spotify
+brew install --cask aware
+brew install --cask postman
+brew install --cask hiddenbar
+brew install --cask the-unarchiver
+brew install --cask vlc
+brew install --cask imageoptim
+brew install --cask paintbrush
