cc2137b0a4f96fc059a96aa06561b1bb18edcc5d
Evan Magoni <esm367@gmail.com>
2021-02-15 13:04:37 -0500

    Config from home computer

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -11,28 +11,28 @@ hs.window.animationDuration = 0;
 ---------------------------------------
 
 -- 1. Define positions
-positions = {
-  maximized = hs.layout.maximized,
-  centered = {x=0.15, y=0.15, w=0.7, h=0.7},
+-- positions = {
+--   maximized = hs.layout.maximized,
+--   centered = {x=0.15, y=0.15, w=0.7, h=0.7},
 
-  left30 = {x=0, y=0, w=0.30, h=1},
-  left50 = hs.layout.left50,
-  left70 = {x=0, y=0, w=0.70, h=1},
+--   left30 = {x=0, y=0, w=0.30, h=1},
+--   left50 = hs.layout.left50,
+--   left70 = {x=0, y=0, w=0.70, h=1},
 
-  right30 = {x=0.70, y=0, w=0.30, h=1},
-  right50 = hs.layout.right50,
-  right70 = {x=0.30, y=0, w=0.70, h=1},
-}
+--   right30 = {x=0.70, y=0, w=0.30, h=1},
+--   right50 = hs.layout.right50,
+--   right70 = {x=0.30, y=0, w=0.70, h=1},
+-- }
 
 -- 2. Map keys to positions
-keysToPositions = {
-  {key="g", unit=positions.left70},
-  {key="h", unit=positions.left50},
-  {key="j", unit=positions.centered},
-  {key="f", unit=positions.maximized},
-  {key="l", unit=positions.right50},
-  {key=";", unit=positions.right70},
-}
+-- keysToPositions = {
+--   {key=";", unit=positions.left50},
+--   {key="'", unit=positions.right50},
+--   {key="[", unit=positions.left70},
+--   {key="]", unit=positions.right70},
+--   {key="f", unit=positions.maximized},
+--   {key="g", unit=positions.centered},
+-- }
 
 -- 3. Bind mappings
 -- https://aaronlasseigne.com/2016/02/16/switching-from-slate-to-hammerspoon/
@@ -40,39 +40,39 @@ function bindKey(key, fn)
   hs.hotkey.bind(hyper, key, fn)
 end
 
-hs.fnutils.each(keysToPositions, function(entry)
-  bindKey(entry.key, function()
-    hs.window.focusedWindow():moveToUnit(entry.unit)
-  end)
-end)
+-- hs.fnutils.each(keysToPositions, function(entry)
+--   bindKey(entry.key, function()
+--     hs.window.focusedWindow():moveToUnit(entry.unit)
+--   end)
+-- end)
 
 ------------------------------------
 -- Hyper i to show window hints
 ------------------------------------
 
-bindKey("i", function()
-    hs.hints.windowHints()
-end)
+-- bindKey("i", function()
+--     hs.hints.windowHints()
+-- end)
 
 ------------------------------------
 -- Move windows between screens
 ------------------------------------
 
-bindKey("left", function()
-  hs.window.focusedWindow():moveOneScreenWest()
-end)
+-- bindKey("left", function()
+--   hs.window.focusedWindow():moveOneScreenWest()
+-- end)
 
-bindKey("right", function()
-  hs.window.focusedWindow():moveOneScreenEast()
-end)
+-- bindKey("right", function()
+--   hs.window.focusedWindow():moveOneScreenEast()
+-- end)
 
-bindKey("down", function()
-  hs.window.focusedWindow():moveOneScreenSouth()
-end)
+-- bindKey("down", function()
+--   hs.window.focusedWindow():moveOneScreenSouth()
+-- end)
 
-bindKey("up", function()
-  hs.window.focusedWindow():moveOneScreenNorth()
-end)
+-- bindKey("up", function()
+--   hs.window.focusedWindow():moveOneScreenNorth()
+-- end)
 
 ------------------------------------
 -- Hyper wasd to switch window focus
@@ -101,51 +101,51 @@ end)
 hs.fnutils.each({
   { key = "b", app = "Firefox" },
   { key = "v", app = "Sourcetree" },
-  { key = "e", app = "Sublime Text" },
-  { key = "c", app = "Code" },
-  { key = "o", app = "Microsoft Outlook" },
-  { key = "t", app = "iTerm2" },
-  { key = "s", app = "Slack" },
+  -- { key = "e", app = "Sublime Text" },
+  { key = "c", app = "Visual Studio Code" },
+  { key = "t", app = "iTerm" },
+  -- { key = "s", app = "Slack" },
   { key = "d", app = "Spotify" },
-  { key = "i", app = "iTunes" },
-  { key = "m", app = "MySQL Workbench" },
-  { key = "y", app = "Things" },
+  { key = "i", app = "Music" },
+  { key = "a", app = "Ableton Live 10 Suite" },
+  { key = "y", app = "Things3" },
 }, function(object)
   bindKey(object.key, function()
-    local app = hs.appfinder.appFromName(object.app)
-    if app then app:activate() end
+    -- local app = hs.appfinder.appFromName(object.app)
+    -- if app then app:activate() end
+    hs.application.launchOrFocus(object.app)
   end)
 end)
 
 ------------------------------------
 ---- Snippet chooser based on https://aldur.github.io/articles/hammerspoon-emojis/
 ------------------------------------
-local choices = hs.json.decode(io.open("snippets.json", "r"):read("*all"))
-
--- Focus the last used window.
-local function focusLastFocused()
-    local wf = hs.window.filter
-    local lastFocused = wf.defaultCurrentSpace:getWindows(wf.sortByFocusedLast)
-    if #lastFocused > 0 then lastFocused[1]:focus() end
-end
-
--- Create the chooser.
--- On selection, copy the emoji and type it into the focused application.
-local chooser = hs.chooser.new(function(choice)
-    -- if not choice then focusLastFocused(); return end
-    if not choice then return end
-    hs.pasteboard.setContents(choice["subText"])
-    -- focusLastFocused()
-    local string = choice["subText"]
-    hs.eventtap.keyStroke({"cmd"}, "v")
-end)
-
-chooser:searchSubText(true)
-chooser:choices(choices)
-
-bindKey("p", function()
-  chooser:show()
-end)
+-- local choices = hs.json.decode(io.open("snippets.json", "r"):read("*all"))
+--
+-- -- Focus the last used window.
+-- local function focusLastFocused()
+--     local wf = hs.window.filter
+--     local lastFocused = wf.defaultCurrentSpace:getWindows(wf.sortByFocusedLast)
+--     if #lastFocused > 0 then lastFocused[1]:focus() end
+-- end
+--
+-- -- Create the chooser.
+-- -- On selection, copy the emoji and type it into the focused application.
+-- local chooser = hs.chooser.new(function(choice)
+--     -- if not choice then focusLastFocused(); return end
+--     if not choice then return end
+--     hs.pasteboard.setContents(choice["subText"])
+--     -- focusLastFocused()
+--     local string = choice["subText"]
+--     hs.eventtap.keyStroke({"cmd"}, "v")
+-- end)
+--
+-- chooser:searchSubText(true)
+-- chooser:choices(choices)
+--
+-- bindKey("p", function()
+--   chooser:show()
+-- end)
 
 ------------------------------------
 ---- Reload config on write
